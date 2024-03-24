5272dfc9ffd9f5eb416734f92034f5cbd3b14fdd
LishaniChamathka <162943365+LishaniChamathka@users.noreply.github.com>
2024-03-24 13:26:28 +0530

    HMS-5 room section update

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Room;
 
 use Illuminate\Http\Request;
 
--- routes/room/room.php
+++ routes/room/room.php
@@ -7,7 +7,7 @@ Route::group(['middleware'=>'auth'],function(){
     Route::group(['prefix'=>'{language}'],function(){
         Route::group(['prefix' =>'room','as'=>'room.'],function(){
             Route::get('view',[RoomController::class,'view'])->name('view');
-            Route::get('list',[RoomController::class,'list'])->name('list');
+            Route::get('list',[RoomController::class,'index'])->name('list');
             Route::get('edit/{id}',[RoomController::class,'edit'])->name('edit');
             Route::get('delete/{id}',[RoomController::class,'delete'])->name('delete');
         });
