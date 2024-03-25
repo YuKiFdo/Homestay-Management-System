9bbaf132a5a986f9c5517f5c9ddeabe0a6d5e4e2
YuKi <yukifernando0001@gmail.com>
2024-03-25 11:37:57 +0530

    create room database migration file

---
+++ database/migrations/2024_03_25_055728_create_room_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('room', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('type');
+            $table->string('bedtype');
+            $table->double('kidprice');
+            $table->double('adultprice');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('room');
+    }
+};
